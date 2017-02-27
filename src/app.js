a2dc421e0aa24421572c70d069e9fe19175fd11d
Tatai Talukder <tataitalukder@gmail.com>
2017-02-28 00:38:21 +0530

    Configuring tests

--- src/app.js
+++ src/app.js
@@ -1,30 +1,7 @@
-import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import './app.css'
-import styles from './styles.module.css'
-import 'font-awesome/css/font-awesome.css'
-
-class App extends Component {
-  render() {
-    return (<div className={styles.wrapper}>
-              <h1> 
-                Text <i className="fa fa-star"></i>
-              </h1> 
-            </div>);
-  }
-}
+import App from 'containers/App/App'
 
 const mountNode = document.querySelector('#root');
 ReactDOM.render(<App />, mountNode);
 
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-
-// const App = React.createClass({
-//   render: function() {
-//     return (<div>Text text text</div>)
-//   }
-// });
-
-// const mountNode = document.querySelector('#root');
-// ReactDOM.render(<App />, mountNode);
