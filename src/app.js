a7de0f4527262b979a6bbc6b445d946007903d30
Tatai Talukder <tataitalukder@gmail.com>
2017-03-07 09:28:08 +0530

    restarted the project

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom'
-import './app.css'
-import App from 'containers/App/App'
-
-const mountNode = document.querySelector('#root');
-ReactDOM.render(<App />, mountNode);
-
